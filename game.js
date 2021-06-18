a74a78471eeae201b5aa0e062671d763750899fe
Andrew Oppo <andrew.oppo@gmail.com>
2021-06-18 11:17:20 +0200

    changd spawnrate again

--- game.js
+++ game.js
@@ -22,8 +22,8 @@ class Game {
         // if (this.song.isPlaying() === false ) {
         //     this.song.play(); 
         // }
-        if (this.spawnRate > 28 && frameCount % 38 === 0) {
-            this.spawnRate -= .12;
+        if (this.spawnRate > 26 && frameCount % 36 === 0) {
+            this.spawnRate -= .17;
         }
         if (frameCount % Math.floor(this.spawnRate) === 0) {
             this.obstacles.push(new Obstacle());
